06e4b577010eadc08435e17483dc1f59c222e0ea
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-11-11 11:20:24 -0500

    multus

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,14 +5,8 @@ set -x RIPGREP_CONFIG_PATH $HOME/.config/rg/config
 set -x HOSTNAME (hostname)
 set -x PNPM_HOME $HOME/.local/share/pnpm
 set -x PATH "$PATH:$PNPM_HOME:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.luarocks/bin"
-set -x GRIM_DEFAULT_DIR $HOME/Media/Pictures/Captures
-set -x PERSONAL $HOME/Personal
 set -x PASSWORD_STORE_DIR $PERSONAL/pass
-
-# musl-only
-set -x LANG "C.UTF-8"
-set -x LC_CTYPE "C.UTF-8"
-# set -x PERSONAL "/data/data/com:termux/files/home/storage/shared/Personal"
+set -x DISPLAY :1
 
 if status is-interactive
   fish_config theme choose Rosé\ Pine
@@ -25,8 +19,8 @@ if status is-interactive
   abbr cme chezmoi edit
   abbr cmcd chezmoi cd
   abbr cmdd chezmoi diff
-	abbr d ddgr -nr
-	abbr dw BROWSER=w3m ddgr -n2
+	abbr d ddgr --rev
+	abbr dw BROWSER=w3m ddgr --rev
   abbr l eza --icons --git
   abbr n nvim
   abbr o xdg-open
@@ -36,9 +30,6 @@ if status is-interactive
   abbr gsc git switch -c
   abbr ytx yt-dlp -x
   abbr yta yt-dlp -x --output "%(autonumber)02d %(title)s.%(ext)s"
-  abbr pi sudo xbps-install -y
-  abbr pr sudo xbps-remove -R 
-  abbr pu sudo xbps-install -Syu
 
   fzf --fish | source
 	starship init fish | source
